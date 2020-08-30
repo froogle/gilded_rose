aec66670166f241a77dffbf68b69096723c101f5
Pete Wright <peterjwright@gmail.com>
2020-08-30 07:52:10 -0400

    Extracted increase and decrease quality into their own methods
    Added two methods to handle increasing and decreasing quality of an item, and refactored main method to use these instead of doing math themselves. 
    
    This lets us start moving as well towards loosely coupled strongly cohesive code - currently update_quality is trying to do too much (code smell)

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,57 +1,60 @@
 class GildedRose
-
   def initialize(items)
     @items = items
+  end
+
+  def increase_item_quality( item )
+    item.quality += 1
+  end    
+
+  def decrease_item_quality( item) 
+    item.quality -= 1 
   end
 
   def update_quality()
     @items.each do |item|
       next if item.name == "Sulfuras, Hand of Ragnaros"
 
-
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           
-            item.quality = item.quality - 1
-            if item.name.start_with?("Conjured")
-              item.quality= item.quality - 1
-            end
-          
+          decrease_item_quality item
+          if item.name.start_with?("Conjured")
+            decrease_item_quality item
+          end
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          increase_item_quality item
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_item_quality item
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                increase_item_quality item
               end
             end
           end
         end
       end
-      
-        item.sell_in = item.sell_in - 1
-      
+
+      item.sell_in = item.sell_in - 1
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-      
-                item.quality = item.quality - 1
-      
+              decrease_item_quality item
             end
           else
             item.quality = item.quality - item.quality
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            increase_item_quality item
           end
         end
       end
