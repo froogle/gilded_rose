7cc6e79020303be9280a153dca904007a1f0186f
Pete Wright <peterjwright@gmail.com>
2020-08-30 06:52:53 -0400

    Add requirements list to specs
    The enhancement is going to require changes to update_quality(), which is a bit of a mess. 
    
    The best route forward when we implement the new conjured items feature is going to require some refactoring, but we can't refactor without tests. 
    
    Added in pending tests to cover all the current system requirements.

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,11 +3,34 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
+    it "should not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "fixme"
     end
+
+    it "should degrade quality twice as fast after sell by date"
+
+    it "should not allow quality to go below zero"
+
+    it "should increase the quality of aged brie over time"
+
+    it "should not increase the quality beyond 50"
+
+    it "should not change the quality of sulfuras"
+
+    it "should allow sulfuras to maintain a quality of 80"
+
+    it "should increase the quality of a backstage pass by 2 ten days before the event"
+
+    it "should increase the quality of a backstage pass by 3 5 days before the event"
+
+    it "should reduce the quality of a backstage pass to 0 after the event"
+
+    it "should degrade the quaklity of a conjured item doubly fast"
+
   end
+
+
 
 end
