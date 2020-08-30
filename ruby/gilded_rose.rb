ba60ce0699c47403ac8c2b3a3ef6cc1401a5a93c
Pete Wright <peterjwright@gmail.com>
2020-08-30 08:51:46 -0400

    Extracted out code to age and expire items
    These two method extractions get us much much closer to where we should be on update_quality. The code is now explicit, clean and simple, with all the complex nested conditionals moved out into their own methods that can now themselves be refactored.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -11,34 +11,39 @@ class GildedRose
     item.quality -= 1 if item.quality > 0
   end
 
+  def age_item( item ) 
+    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+      decrease_item_quality item
+      decrease_item_quality item if item.name.start_with?("Conjured")
+    else
+      increase_item_quality item
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        increase_item_quality item if item.sell_in < 11
+        increase_item_quality item if item.sell_in < 6
+      end
+    end
+  end
+
+  def expire_item(item)
+    if item.name != "Aged Brie"
+      if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        decrease_item_quality item
+      else
+        item.quality = item.quality - item.quality
+      end
+    else
+      increase_item_quality item
+    end
+  end
+
   def update_quality()
     @items.each do |item|
       next if item.name == "Sulfuras, Hand of Ragnaros"
-
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        decrease_item_quality item
-        decrease_item_quality item if item.name.start_with?("Conjured")
-      else
-        increase_item_quality item
-        if item.name == "Backstage passes to a TAFKAL80ETC concert"
-          increase_item_quality item if item.sell_in < 11
-          increase_item_quality item if item.sell_in < 6
-        end
-      end
-
+      age_item item
+      
       item.sell_in = item.sell_in - 1
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            decrease_item_quality item
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          increase_item_quality item
-        end
-      end
+      
+      expire_item item if item.sell_in < 0
     end
   end
 end
