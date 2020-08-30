4938381d56eb49ce04bdb9ca87153d48046ebb08
Pete Wright <peterjwright@gmail.com>
2020-08-30 07:14:03 -0400

    Added first spec, and refactored
    Added the first spec to check the rate at which quality degrades after the sell by date. 
    
    Spotted a lot of duplication already in the two tests, and refactored to bring the specs down to single line assertions.

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,15 +1,23 @@
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
+def updated_item( name:, sell_by:, quality: )
+  items = [Item.new(name, sell_by, quality)]
+  GildedRose.new(items).update_quality()
+  items[0]
+end
+
+
+
 describe GildedRose do
 
   describe "#update_quality" do
     it "should not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(updated_item(name: "foo", sell_by: 0, quality: 0).name).to eq "foo"
     end
 
-    it "should degrade quality twice as fast after sell by date"
+    it "should degrade quality twice as fast after sell by date" do 
+      expect(updated_item(name: "something", sell_by: -1, quality: 10).quality).to eq 8
+    end
 
     it "should not allow quality to go below zero"
 
@@ -23,11 +31,11 @@ describe GildedRose do
 
     it "should increase the quality of a backstage pass by 2 ten days before the event"
 
-    it "should increase the quality of a backstage pass by 3 5 days before the event"
+    it "should increase the quality of a backstage pass by 3 five days before the event"
 
     it "should reduce the quality of a backstage pass to 0 after the event"
 
-    it "should degrade the quaklity of a conjured item doubly fast"
+    it "should degrade the quality of a conjured item doubly fast"
 
   end
 
