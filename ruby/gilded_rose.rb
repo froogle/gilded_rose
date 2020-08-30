c9652f86acaa413831cd45c4a737e1ccef658bc1
Pete Wright <peterjwright@gmail.com>
2020-08-30 09:10:31 -0400

    Removed all magic numbers and strings
    Removed all the literal numbers and strings in the code to reduce the risk of bugs later, and make the code more explicit.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,49 +1,70 @@
 class GildedRose
+
+  MINIMUM_QUALITY = 0 
+  MAXIMUM_QUALITY = 50
+  TEN_DAYS = 10
+  FIVE_DAYS = 5
+
+  BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+  AGED_BRIE = "Aged Brie"
+  SUFURAS = "Sulfuras, Hand of Ragnaros"
+  CONJURED_ITEM = "Conjured"
+
   def initialize(items)
     @items = items
   end
 
+  def update_quality()
+    @items.each do |item|
+      next if item.name == SULFURAS
+      
+      age_item item
+      item.sell_in -= 1
+      expire_item item if past_sell_by? item
+    end
+  end
+
+private
+
+  def conjured_item?(item)
+    item.name.start_with?(CONJURED_ITEM)
+  end
+
+
+  def past_sell_by?(item)
+    item.sell_in < 0
+  end
+
   def increase_item_quality(item)
-    item.quality += 1 if item.quality < 50
+    item.quality += 1 if item.quality < MAXIMUM_QUALITY
   end
 
   def decrease_item_quality(item)
-    item.quality -= 1 if item.quality > 0
+    item.quality -= 1 if item.quality > MINIMUM_QUALITY
   end
 
-  def age_item( item ) 
-    if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+  def age_item(item)
+    if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS
       decrease_item_quality item
-      decrease_item_quality item if item.name.start_with?("Conjured")
+      decrease_item_quality item if conjured_item? item
     else
       increase_item_quality item
-      if item.name == "Backstage passes to a TAFKAL80ETC concert"
-        increase_item_quality item if item.sell_in < 11
-        increase_item_quality item if item.sell_in < 6
+      if item.name == BACKSTAGE_PASS
+        increase_item_quality item if item.sell_in <= TEN_DAYS
+        increase_item_quality item if item.sell_in <= FIVE_DAYS
       end
     end
   end
 
   def expire_item(item)
-    if item.name != "Aged Brie"
-      if item.name != "Backstage passes to a TAFKAL80ETC concert"
+    if item.name != AGED_BRIE
+      if item.name != BACKSTAGE_PASS
         decrease_item_quality item
       else
         item.quality = item.quality - item.quality
       end
     else
       increase_item_quality item
-    end
-  end
-
-  def update_quality()
-    @items.each do |item|
-      next if item.name == "Sulfuras, Hand of Ragnaros"
-      age_item item
-      
-      item.sell_in = item.sell_in - 1
-      
-      expire_item item if item.sell_in < 0
     end
   end
 end
