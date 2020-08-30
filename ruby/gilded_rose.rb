10c0f32970d210c3f50a372e57ed5b5797c6940c
Pete Wright <peterjwright@gmail.com>
2020-08-30 07:48:13 -0400

    Removed all checks for Sulfuras
    We can skip the loop if we have Sulfuras. For now just added in a next to the start of the loop if this is the item we are working with. Can optimize this more later in the refactoring with .select and .map

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -6,14 +6,17 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
+      next if item.name == "Sulfuras, Hand of Ragnaros"
+
+
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
+          
             item.quality = item.quality - 1
             if item.name.start_with?("Conjured")
               item.quality= item.quality - 1
             end
-          end
+          
         end
       else
         if item.quality < 50
@@ -32,16 +35,16 @@ class GildedRose
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
+      
         item.sell_in = item.sell_in - 1
-      end
+      
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+      
                 item.quality = item.quality - 1
-              end
+      
             end
           else
             item.quality = item.quality - item.quality
