b1b3298d6df988b05a5efab4a909f5b155ecda86
Pete Wright <peterjwright@gmail.com>
2020-08-30 09:29:10 -0400

    Removed all negative logic and cleaned up code
    The final step, removed all negative conditional checks. Also built out a number of short conditional methods to encapsulate business rules and almost build a DSL to make the code much more readable at a high level.

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,6 +1,7 @@
 class GildedRose
 
-  MINIMUM_QUALITY = 0 
+  #========== constants
+  MINIMUM_QUALITY = 0
   MAXIMUM_QUALITY = 50
   TEN_DAYS = 10
   FIVE_DAYS = 5
@@ -14,22 +15,47 @@ class GildedRose
     @items = items
   end
 
+  #========== update quality of an item over time
   def update_quality()
-    @items.select {|item| item.name != SULFURAS}.each do |item|
+    @items.select { |item| item.name != SULFURAS }.each do |item|
       age_item item
       item.sell_in -= 1
       expire_item item if past_sell_by? item
     end
   end
 
-private
-
-  def conjured_item?(item)
-    item.name.start_with?(CONJURED_ITEM)
+  
+  
+  #========== item quality methods
+  private
+  def age_item(item)
+    if item_improves_with_age? item
+      improve_item item
+    else
+      degrade_item item
+    end
   end
 
-  def past_sell_by?(item)
-    item.sell_in < 0
+  def expire_item(item)
+    if item_improve_after_expiry? item
+      increase_item_quality item
+    else
+      decrease_item_quality item
+      item.quality = MINIMUM_QUALITY if item_worthless_after_expiry item
+    end
+  end
+
+  def improve_item(item)
+    increase_item_quality item
+    if item.name == BACKSTAGE_PASS
+      increase_item_quality item if item.sell_in <= TEN_DAYS
+      increase_item_quality item if item.sell_in <= FIVE_DAYS
+    end
+  end
+
+  def degrade_item(item)
+    decrease_item_quality item
+    decrease_item_quality item if conjured_item? item
   end
 
   def increase_item_quality(item)
@@ -40,29 +66,25 @@ private
     item.quality -= 1 if item.quality > MINIMUM_QUALITY
   end
 
-  def age_item(item)
-    if item.name != AGED_BRIE and item.name != BACKSTAGE_PASS
-      decrease_item_quality item
-      decrease_item_quality item if conjured_item? item
-    else
-      increase_item_quality item
-      if item.name == BACKSTAGE_PASS
-        increase_item_quality item if item.sell_in <= TEN_DAYS
-        increase_item_quality item if item.sell_in <= FIVE_DAYS
-      end
-    end
+  #========= business rules conditions
+  def item_improves_with_age?(item)
+    item.name == AGED_BRIE || item.name == BACKSTAGE_PASS
   end
 
-  def expire_item(item)
-    if item.name != AGED_BRIE
-      if item.name != BACKSTAGE_PASS
-        decrease_item_quality item
-      else
-        item.quality = item.quality - item.quality
-      end
-    else
-      increase_item_quality item
-    end
+  def item_improve_after_expiry?(item)
+    item.name == AGED_BRIE
+  end
+
+  def item_worthless_after_expiry(item)
+    item.name == BACKSTAGE_PASS
+  end
+
+  def conjured_item?(item)
+    item.name.start_with?(CONJURED_ITEM)
+  end
+
+  def past_sell_by?(item)
+    item.sell_in < 0
   end
 end
 
